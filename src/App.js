ff99e62f243982eef26344a2c1bb32062fcba8a1
William <william1234562@gmail.com>
2017-10-28 01:52:40 +0800

    add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import Login from './Login';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      speed: 10,
+      user: undefined
+    }
+  }
+
+  componentDidMount() {
+    const database = firebase.database();
+    const reactRef = database.ref().child('react');
+    const speedRef = reactRef.child('speed');
+    speedRef.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      })
+    console.log(snap.val())
+    })
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        console.log(firebaseUser);
+        this.setState({
+          user: firebaseUser
+        })
+      } else {
+        console.log('not logged in');
+        this.setState({
+          user: undefined
+        })
+      }
+    })
+  }
+
+  login = (event) => {
+    event.preventDefault();
+    const {email, password} = event.target;
+    console.log(email.value);
+    console.log(password.value);
+
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => console.log(e.message))
+  }
+
+  logout = () => {
+    firebase.auth().signOut();
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React, Hello World!!!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='App-intro'>
+          <p>{this.state.speed}</p>
+          {this.state.user ? 
+            <button id='logoutBtn' onClick={this.logout}>Logout</button>
+           : 
+           <Login login={this.login}/> 
+          }
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBpjcIcvwV53njlvJNUN-bi2zQpAWSoT-8",
+    authDomain: "random-list-d7e72.firebaseapp.com",
+    databaseURL: "https://random-list-d7e72.firebaseio.com",
+    projectId: "random-list-d7e72",
+    storageBucket: "",
+    messagingSenderId: "754150436541"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
